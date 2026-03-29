11d0658e5164251f038cc2ca22e18d2732ff65ac
MilSoltani <miladsoltani@outlook.de>
2026-03-29 07:17:37 +0200

    chore: installed shadcn

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import Button from '@web/components/ui/button/Button.vue'
 import {UserClient} from '@teacket/api/clients'
 
 UserClient.index.$get()
 </script>
 
 <template>
+  <Button>Hello!</Button>
 </template>
